fc16e73545871a082bfa46f453e5ede3abcea496
oliverbirchill <obirchill@gmail.com>
2026-06-03 16:05:21 +0100

    refactored functions

--- main.py
+++ main.py
@@ -5,17 +5,55 @@ suits = ["♤", "♡", "♢", "♧"]
 raise_range = {"AA", "QQ", "AKs", "AQs", "AKo"}
 options = {"raise", "fold"}
 
-def generate_hand():
-    rank_1 = random.choice(ranks)
-    rank_2 = random.choice(ranks)
+def generate_card():
+    rank = random.choice(ranks)
+    suit = random.choice(suits)
+    return rank, suit
 
-    if rank_1 == rank_2:
-        return rank_1 + rank_2
+def get_hand_notation(hand):
+    hand_notation = ""
+
+    first_card = hand[0]
+    second_card = hand[1]
+
+    first_rank = first_card[0]
+    first_suit = first_card[1]
+
+    second_rank = second_card[0]
+    second_suit = second_card[1]
+
+    if first_rank == second_rank:
+        hand_notation = first_rank + second_rank
+    elif first_suit == second_suit:
+        hand_notation = first_rank + second_rank + "s"
     else:
-        hand = "".join(sorted(rank_1 + rank_2, key=ranks.index, reverse=True))
-        suit_or_offsuit = random.choice(["s", "o"])
-        return hand + suit_or_offsuit
+        hand_notation = first_rank + second_rank + "o"
+
+    return hand_notation
+
+def generate_hand():
+    card_1 = generate_card()
+    card_2 = generate_card()
+
+    while card_2 == card_1:
+        card_2 = generate_card()
+
+    hand = card_1, card_2
+    hand = sorted(hand, key=lambda card: ranks.index(card[0]), reverse=True)
+
+    hand_notation = get_hand_notation(hand)
+
+    return hand, hand_notation
     
+def display_hand(hand):
+    first_card = hand[0]
+    second_card = hand[1]
+
+    first_card_display = first_card[0] + first_card[1]
+    second_card_display = second_card[0] + second_card[1]
+
+    return first_card_display + " " + second_card_display
+
 def get_number_of_questions():
     while True:
         answer = input("How many questions would you like me to ask? ")
@@ -32,8 +70,8 @@ def get_number_of_questions():
 
         return number
 
-def get_correct_action(hand):
-    return "raise" if hand in raise_range else "fold"
+def get_correct_action(hand_notation):
+    return "raise" if hand_notation in raise_range else "fold"
 
 def normalize_answer(answer):
     answer = answer.lower().strip()
@@ -49,20 +87,21 @@ score = 0
 number_of_questions = get_number_of_questions()
 
 for question_number in range(number_of_questions):
-    hand = generate_hand()
-    user_answer = normalize_answer(input(f"{hand}. Raise or fold? "))
+    hand, hand_notation = generate_hand()
+    display = display_hand(hand)
+    user_answer = normalize_answer(input(f"{display}. Raise or fold? "))
 
     while user_answer not in options:
         print("That doesn't look to be a correct input... Please enter raise or fold.")
-        user_answer = normalize_answer(input(f"{hand}. Raise or fold? "))
+        user_answer = normalize_answer(input(f"{display}. Raise or fold? "))
 
-    correct_action = get_correct_action(hand)
+    correct_action = get_correct_action(hand_notation)
 
     if user_answer == correct_action:
         score += 1
         print(f"Correct! Your score is now {score}.")
     else:
-        print(f"Incorrect! According to your ranges, you should {correct_action} {hand}. Your score is {score}.")
+        print(f"Incorrect! According to your ranges, you should {correct_action} {display}. Your score is {score}.")
             
 print(f"Your final score is {score}/{number_of_questions} or {round(score / number_of_questions * 100)}%.")
 
